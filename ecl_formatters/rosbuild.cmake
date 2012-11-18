bd0ee6bb59fc19561794616dd75a89ee4a3ed561
Daniel Stonier <d.stonier@gmail.com>
2012-11-19 01:53:21 +0900

    ecl_formatters catkinized.

--- ecl_formatters/rosbuild.cmake
+++
@@ -1,9 +0,0 @@
-if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-  include(${CMAKE_CURRENT_BINARY_DIR}/package.cmake)
-endif()
-
-include(${ecl_build_SOURCE_DIR}/cmake/modules/ecl_package.cmake)
-
-add_subdirectory(include)
-add_subdirectory(src)
-add_subdirectory(pkg-config)
