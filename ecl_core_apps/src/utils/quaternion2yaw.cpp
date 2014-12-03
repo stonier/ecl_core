b0f5cf2f97234f64cb5c93c6776b3ae3239e495d
Daniel Stonier <d.stonier@gmail.com>
2014-12-03 16:49:52 +0900

    [ecl_core_apps] quaternion2yaw utility, closes #40.

--- ecl_core_apps/src/utils/quaternion2yaw.cpp
+++ ecl_core_apps/src/utils/quaternion2yaw.cpp
@@ -1,5 +1,5 @@
 /**
- * @file /src/utils/yaw2quaternion.cpp
+ * @file /src/utils/quaternion2yaw.cpp
  *
  * @brief Angle converter.
  **/
@@ -11,6 +11,7 @@
 #include <iostream>
 #include <ecl/command_line.hpp>
 #include <ecl/linear_algebra.hpp>
+#include <ecl/math.hpp>
 
 /*****************************************************************************
  * Using
@@ -29,19 +30,29 @@ int main(int argc, char** argv) {
      * Parse for the port name
      ******************************************/
     bool hex(false);
-    double yaw;
+    double x, y, z, w;
 
     try {
         CmdLine cmd("Calculator for yaw to quaternion.",' ',"0.1");
-        UnlabeledValueArg<double> arg_yaw("yaw","Yaw angle to convert",true,0.0,"float", cmd);
+        UnlabeledValueArg<double> arg_x("x","Quaternion component - x",true,0.0,"float", cmd);
+        UnlabeledValueArg<double> arg_y("y","Quaternion component - y",true,0.0,"float", cmd);
+        UnlabeledValueArg<double> arg_z("z","Quaternion component - z",true,0.0,"float", cmd);
+        UnlabeledValueArg<double> arg_w("w","Quaternion component - w",true,0.0,"float", cmd);
         cmd.parse(argc,argv);
-        yaw = arg_yaw.getValue();
+        x = arg_x.getValue();
+        y = arg_y.getValue();
+        z = arg_z.getValue();
+        w = arg_w.getValue();
     } catch ( ArgException &e ) {
         std::cerr << "error: " << e.error() << " for arg " << e.argId() << std::endl;
     }
-    Eigen::Quaternion<double> q;
-    q = Eigen::AngleAxis<double>(yaw, Eigen::Vector3d::UnitZ());
-    std::cout << "Quaternion: [" << q.x() << " " << q.y() << " " << q.z() << " " << q.w() << "]" << std::endl;
+    if ( ( x != 0.0 ) && ( y != 0.0 ) ) {
+      std::cout << "This quaternion has non-yaw components, aborting." << std::endl;
+    }
+    double rads = 2*acos(w);
+    double degrees = rads * 360.0 / (2 * ecl::pi);
+    std::cout << "Quaternion {" << x << ", " << y << ", " << z << ", " << w << "} -> ";
+    std::cout << rads << " radians -> " << degrees << " degrees." << std::endl;
 
     return 0;
 }
