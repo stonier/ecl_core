7d0c4b57b822563f8149e445eab6cfbce66247f7
Daniel Stonier <d.stonier@gmail.com>
2015-09-06 14:03:37 +0900

    [ecl_threads] skip deadlock test in release mode, posix doesn't help us.

--- ecl_threads/src/test/mutex.cpp
+++ ecl_threads/src/test/mutex.cpp
@@ -9,15 +9,15 @@
  * @date June 2009
  **/
 /*****************************************************************************
-** Platform Check
-*****************************************************************************/
+ ** Platform Check
+ *****************************************************************************/
 #include <iostream>
 #include <ecl/config/ecl.hpp>
 #if defined(ECL_IS_POSIX)
 
 /*****************************************************************************
-** Includes
-*****************************************************************************/
+ ** Includes
+ *****************************************************************************/
 
 #include <iostream>
 #include <gtest/gtest.h>
@@ -27,77 +27,77 @@
 #include "../../include/ecl/threads/mutex.hpp"
 
 /*****************************************************************************
-** Using
-*****************************************************************************/
+ ** Using
+ *****************************************************************************/
 
 using ecl::StandardException;
 using ecl::Mutex;
 using ecl::Duration;
 
 /*****************************************************************************
-** Macros
-*****************************************************************************/
+ ** TESTS
+ *****************************************************************************/
 
-// Make sure we can test the debug mode properties.
-#if defined(NDEBUG) || defined(ECL_NDEBUG)
-	#undef NDEBUG
-	#undef ECL_NDEBUG
-#endif
-
-/*****************************************************************************
-** TESTS
-*****************************************************************************/
-
-TEST(MutexTests,selfDestruct) {
-	// It will now attempt to self-destruct while locked and throw an exception.
-	Mutex mutex;
+TEST(MutexTests,selfDestruct)
+{
+  #if defined(NDEBUG) || defined(ECL_NDEBUG)
+    std::cout << "Skipping deadlock tests - posix doesn't help you in release mode." << std::endl;
+    SUCCEED();
+  #else
+    // It will now attempt to self-destruct while locked and throw an exception.
+    // This behaviour is only provided by posix in debug mode.
+    Mutex mutex;
     mutex.lock();
     try {
-    	mutex.lock();
-    } catch ( StandardException &e ) {
-    	SUCCEED();
+      mutex.lock();
+    } catch (StandardException &e) {
+      SUCCEED();
     }
     mutex.unlock();
+  #endif
 }
 
-TEST(MutexTests,tryLock) {
-	// It will now attempt to self-destruct while locked and throw an exception.
-	Mutex mutex;
-    mutex.trylock();
-    EXPECT_FALSE(mutex.trylock());
-    mutex.unlock();
+TEST(MutexTests,tryLock)
+{
+  // It will now attempt to self-destruct while locked and throw an exception.
+  Mutex mutex;
+  mutex.trylock();
+  EXPECT_FALSE(mutex.trylock());
+  mutex.unlock();
 }
 
-TEST(MutexTests,timedLock) {
-	// It will now attempt to self-destruct while locked and throw an exception.
-	// Trying to activate from the same thread will cause a deadlock
-	// and thus exception. Need to make a test with different threads here.
-	// Duration duration(1,500000000);
-	// Mutex mutex;
-	// mutex.lock();
-	// mutex.timedlock(duration);
+TEST(MutexTests,timedLock)
+{
+  // It will now attempt to self-destruct while locked and throw an exception.
+  // Trying to activate from the same thread will cause a deadlock
+  // and thus exception. Need to make a test with different threads here.
+  // Duration duration(1,500000000);
+  // Mutex mutex;
+  // mutex.lock();
+  // mutex.timedlock(duration);
 }
 
 /*****************************************************************************
-** Main program
-*****************************************************************************/
+ ** Main program
+ *****************************************************************************/
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-    testing::InitGoogleTest(&argc,argv);
-    return RUN_ALL_TESTS();
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
-
 
 #else
 
 /*****************************************************************************
-** Alternative main
-*****************************************************************************/
+ ** Alternative main
+ *****************************************************************************/
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
-	std::cout << "Currently not supported on your platform." << std::endl;
+  std::cout << "Currently not supported on your platform." << std::endl;
 }
 
 #endif /* ECL_IS_POSIX */
