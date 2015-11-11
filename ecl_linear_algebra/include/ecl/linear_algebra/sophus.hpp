83adfa590e6ecb45992546ddf215058139a82179
Daniel Stonier <d.stonier@gmail.com>
2015-11-12 00:29:48 +0900

    [ecl_linear_algebra] included sophus, err not...now is.

--- ecl_linear_algebra/include/ecl/linear_algebra/sophus.hpp
+++ ecl_linear_algebra/include/ecl/linear_algebra/sophus.hpp
@@ -12,6 +12,11 @@
 ** Includes
 *****************************************************************************/
 
+#include <sophus/se3.hpp>
+#include <sophus/se2.hpp>
+#include <sophus/so2.hpp>
+#include <sophus/so3.hpp>
+
 #include "sophus/helpers.hpp"
 #include "sophus/formatters.hpp"
 #include "sophus/interpolators.hpp"
