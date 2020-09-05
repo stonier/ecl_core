1f89a4b635eff374ba04529fae4fa008ad9531bb
Daniel Stonier <d.stonier@gmail.com>
2020-09-05 19:50:20 -0400

    [infra] eclipse settings

---
+++ ecl_geometry/src/lib/pose.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file /src/lib/pose.cpp
+ *
+ * @brief Implementation of the pose library suite.
+ *
+ * Implementation of the pose library suite.
+ **/
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include "../../include/ecl/geometry/pose.hpp"
+
+/*****************************************************************************
+** Namespaces
+*****************************************************************************/
+
+namespace ecl {
+namespace pose {
+
+ecl::linear_algebra::Vector3d add(
+    const ecl::linear_algebra::Vector3d& pose_1,)
+    const ecl::linear_algebra::Vector3d& pose_2
+) {
+  return ecl::linear_algebra::Vector3d;
+}
+
+
+} // namespace pose
+} // namespace ecl
